f967ba11892b7681a807aee1558f385957a6acad
Raylla Ruthiely <rayllaruthielly707616@gmail.com>
2026-03-18 23:03:55 -0300

    Adicionei n3

--- CalculoMedia/src/calculomedia/Media.java
+++ CalculoMedia/src/calculomedia/Media.java
@@ -6,13 +6,15 @@ public class Media {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		double n1, n2, media;
+		double n1, n2, n3, media;
 		
 		System.out.print("Digite o primeiro numero: ");
 		n1 = scanner.nextDouble();
 		System.out.print("Digite o segundo numero: ");
 		n2 = scanner.nextDouble();
-		media = (n1 + n2)/ 2;
+		System.out.print("Digite o terceiro numero: ");
+		n3 = scanner.nextDouble();
+		media = (n1 + n2 + n3)/ 3;
 		System.out.println("Media: " + media);
 		scanner.close();
 	}
